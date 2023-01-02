f7eac4fbd3bf961b9887dc3d570a73bd15737c35
Ismail Ibrahim <opeyemi942@gmail.com>
2023-01-02 07:17:11 -0800

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * cap_spring - capitalizes all word of a string
+ * @str: the string to be capitalize
+ * Return: a pointer to the changed string
+ */
+
+char *cap_string(char *str)
+
+{
+
+	int index = 0;
+
+	while (str[index])
+	{
+	while (!(str[index] >= '0' && str[index] <= 'z'))
+	index++;
+	if (str[index - 1] == ' ' ||
+	str[index - 1] == '\t' ||
+	str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+	str[index - 1] == ';' ||
+	str[index - 1] == '.' ||
+	str[index - 1] == '!' ||
+	str[index - 1] == '?' ||
+	str[index - 1] == '"' ||
+	str[index - 1] == '(' ||
+	str[index - 1] == ')' ||
+	str[index - 1] == '{' ||
+	str[index - 1] == '}' ||
+	inde == 0)
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+}
+
